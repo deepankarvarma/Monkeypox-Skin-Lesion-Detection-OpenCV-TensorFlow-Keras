2431ef113f33143fcdf34bc25a45a10061fe33e7
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-14 11:15:29 +0530

    added minors

--- model.py
+++ model.py
@@ -49,6 +49,6 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train model
-model.fit(X_train, y_train, epochs=40, batch_size=32, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
 model.save('monkeypox_model.h5')
